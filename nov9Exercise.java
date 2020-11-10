36f1df5175c046055a9d6c61928555211a8eb859
Sanjay Chandrasekar <sanjaychandrasekar@sanjays-mbp.lan>
2020-11-09 19:32:55 -0800

    completed

---
+++ nov9Exercise.java
@@ -0,0 +1,13 @@
+public class nov9Exercise {
+    public static int x = 20;
+    public static int y = 4;
+
+    public static void main(String[] args) {
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x / y);
+        System.out.println(x * y);
+        
+        System.out.println("Sanjay Chandrasekar");
+    }
+}
